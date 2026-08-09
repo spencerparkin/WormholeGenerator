cdfe852e82bab592c63e548a5a6815f328768ad5
Spencer Parkin <spencer.parkin@proton.me>
2026-08-09 10:26:54 -0600

    Makes more sense to just leverage an "engine" I already wrote that sits on top of DX11.

--- Simulation/Source/App.h
+++ Simulation/Source/App.h
@@ -1,47 +1,16 @@
 #pragma once
 
-#include <Windows.h>
-#include <d3d11.h>
+#include "Game.h"
 #include "WormholeGenerator/WormholeTree.h"
 
-template<typename T>
-void SafeRelease(T*& thing)
-{
-	if (thing)
-	{
-		thing->Release();
-		thing = nullptr;
-	}
-}
-
-#define WINDOW_CLASS_NAME	TEXT("WormholeSimulationWindowClass")
-
-class App
+class App : public Imzadi::Game
 {
 public:
 	App(HINSTANCE instance);
 	virtual ~App();
 
-	bool Setup();
-	bool Run(double deltaTime);
-	bool Shutdown();
+	virtual bool PostInit() override;
 
 private:
-	LRESULT WndProc(UINT msg, WPARAM wParam, LPARAM lParam);
-
-	static LRESULT CALLBACK WndProcEntryFunc(HWND windowHandle, UINT msg, WPARAM wParam, LPARAM lParam);
-
-	bool RecreateViews();
-	void Render();
-
-	HINSTANCE instance;
-	HWND windowHandle;
-	ID3D11Device* device;
-	ID3D11DeviceContext* deviceContext;
-	IDXGISwapChain* swapChain;
-	ID3D11RenderTargetView* frameBufferView;
-	ID3D11DepthStencilView* depthStencilView;
-	bool windowResized;
-	D3D11_VIEWPORT viewport;
 	WormholeGenerator::WormholeTree wormholeTree;
 };
--- Simulation/Source/Main.cpp
+++ Simulation/Source/Main.cpp
@@ -1,29 +1,19 @@
 #include "Main.h"
 #include "App.h"
-#include <Windows.h>
-#include <chrono>
 
-int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevIntance, LPSTR cmdLine, int showCmd)
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmdLine, int showCmd)
 {
 	App app(instance);
+	App::Set(&app);
 
-	if (!app.Setup())
-		return 1;
-
-	auto lastTime = std::chrono::steady_clock::now();
-
-	while (true)
+	if (app.Initialize())
 	{
-		auto currentTime = std::chrono::steady_clock::now();
-		std::chrono::duration<double> elapsedTime{ currentTime - lastTime };
-		lastTime = currentTime;
-
-		if (!app.Run(elapsedTime.count()))
-			break;
+		while (app.Run())
+		{
+		}
 	}
 
-	if (!app.Shutdown())
-		return 1;
+	app.Shutdown();
 
 	return 0;
 }
